b7c55a42156585dd36aab4803c64592f2e49640a
karasevmain <karasevmain@gmail.com>
2024-04-01 18:11:43 +0300

    fix game list

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,24 +7,25 @@ public class App {
         String[] gameList = {"Exit", "Greet", "Even"};
         int[] evenGameNumbers = {38, 35, 91};
         System.out.println("Please enter the game number and press Enter.");
-        int selectedGame = sendGameList(gameList);
+        String selectedGame = sendGameList(gameList);
         switch (selectedGame) {
-            case 0:
+            case "0":
                 break;
-            case 1:
+            case "1":
                 Games.playGreet();
-            case 2:
+            case "2":
                 Games.playEven(evenGameNumbers);
-            default: break;
+            default:
+                System.out.println("Error command number");
+                break;
         }
     }
-    public static int sendGameList(String[] gameList) {
+    public static String sendGameList(String[] gameList) {
         for (int i = 0; i < gameList.length; i++) {
             System.out.println(i + " - " + gameList[i]);
         }
         System.out.print("Your choice: ");
         Scanner scanner = new Scanner(System.in);
-        String gameName = scanner.nextLine();
-        return Integer.parseInt(gameName);
+        return scanner.nextLine();
     }
 }
