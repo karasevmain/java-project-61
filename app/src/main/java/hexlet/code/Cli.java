966a72b386acf94173f66edbf56bf8a543f3b8df
karasevmain <karasevmain@gmail.com>
2024-04-01 14:06:28 +0300

    Add input method

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void welcomeMessage() {
+        System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+    }
+
+}
