ec972326c78358aef24eb77c910c10d953950f5d
karasevmain <karasevmain@gmail.com>
2024-04-03 17:06:02 +0300

    change count round

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         String[] gameList = {"Exit", "Greet", "Even", "Calc", "GCD", "Progression", "Prime"};
-        int gameRounds = 3;
+        int gameRounds = 2;
         System.out.println("Please enter the game number and press Enter.");
         String selectedGame = sendGameList(gameList);
         switch (selectedGame) {
